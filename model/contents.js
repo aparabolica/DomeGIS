45574c74c78ecfd4f81836083464a84c7d25017f
Vitor George <vitor.george@gmail.com>
2016-03-16 16:15:27 -0300

    sync content data

--- model/contents.js
+++ model/contents.js
@@ -1,1 +1,11 @@
 Contents = new Mongo.Collection("contents");
+
+Meteor.methods({
+  syncContent: function(content){
+
+    content.syncedAt = Date.now();
+
+    Contents.update({id: content.id}, { $set: { data: content } }, { upsert: true });
+
+  }
+});
