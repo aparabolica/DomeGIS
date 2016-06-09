f6ef5a04255a7767835b06fb010bf7248296e3cf
Vitor George <vitor.george@gmail.com>
2016-06-09 13:18:37 -0300

    remove unused logger

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,7 +2,6 @@
 
 var handler = require('feathers-errors/handler');
 var notFound = require('./not-found-handler');
-var logger = require('./logger');
 
 module.exports = function() {
   // Add your custom middleware here. Remember, that
@@ -11,6 +10,5 @@ module.exports = function() {
   var app = this;
 
   app.use(notFound());
-  app.use(logger(app));
   app.use(handler());
 };
