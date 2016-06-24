c0cb033f95debdb72bb2fb72eb0938c6d0e026b9
Miguel Peixe <miguel@cardume.art.br>
2016-06-24 13:07:33 -0300

    body display none before document ready

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,6 @@ angular.element(document).ready(function() {
   if(self != top) {
     angular.element('body').addClass('iframe');
   }
-  angular.element('body').css({display:'block'});
   angular.bootstrap(document, ['domegis']);
+  angular.element('body').css({display:'block'});
 });
