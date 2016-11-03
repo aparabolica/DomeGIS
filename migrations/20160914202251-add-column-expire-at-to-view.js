c9271b6f890e25e7cca3432fa411cd8ea5379f5d
Vitor George <vitor.george@gmail.com>
2016-11-03 15:10:28 -0200

    sequelize migration should return query result

--- migrations/20160914202251-add-column-expire-at-to-view.js
+++ migrations/20160914202251-add-column-expire-at-to-view.js
@@ -2,10 +2,10 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    queryInterface.addColumn('views', 'expiresAt', Sequelize.DATE )
+    return queryInterface.addColumn('views', 'expiresAt', Sequelize.DATE )
   },
 
   down: function (queryInterface, Sequelize) {
-    queryInterface.removeColumn('views', 'expiresAt')
+    return queryInterface.removeColumn('views', 'expiresAt')
   }
 };
