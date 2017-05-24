53bb48888faa54ccea68a34c10ea9a4c62d20d84
Vitor George <vitor.george@aparabolica.com.br>
2017-05-23 21:33:15 -0300

    Improve PostGIS Dockerfile

--- docker/postgis/setup-database.sh
+++ docker/postgis/setup-database.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-
 set -e
 
-# Perform all actions as $POSTGRES_USER
-export POSTGRES_DB="$POSTGRES_DB"
+psql -v ON_ERROR_STOP=1 --dbname="$POSTGRES_DB" --username "$POSTGRES_USER" <<-EOSQL
+  CREATE USER domegis_readonly WITH PASSWORD 'domegis';
+  GRANT CONNECT ON DATABASE domegis TO domegis;
+  GRANT USAGE ON SCHEMA public TO domegis;
 
-# Load PostGIS into both template_database and $POSTGRES_DB
-for DB in "$POSTGRES_DB"; do
-	echo "Loading Carto extensions into $DB"
-	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
-    CREATE USER domegis_readonly WITH PASSWORD 'domegis';
-    GRANT CONNECT ON DATABASE domegis TO domegis;
-    GRANT USAGE ON SCHEMA public TO domegis;
+  CREATE EXTENSION schema_triggers;
+  CREATE EXTENSION plpythonu;
+  CREATE EXTENSION cartodb;
 EOSQL
-done
