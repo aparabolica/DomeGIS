34f9481ad457d730137dc44c7962748e253f61da
Vitor George <vitor.george@aparabolica.com.br>
2017-06-12 15:56:00 -0300

    Make test suite wait for default user to be created

--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var chai = require('chai');
+
 // init app (if not done already)
 before(function(doneBefore){
   var self = this;
@@ -10,7 +12,36 @@ before(function(doneBefore){
     global.app = require('../src/app');
     self.server = app.listen(3030);
     self.server.once('listening', function() {
-      doneBefore();
+
+      var remainingAttempts = 5;
+
+      function login() {
+
+        // login as admin
+        chai.request(app)
+            .post('/auth/local')
+            .set('Accept', 'application/json')
+            .send({
+               'email': 'admin@domegis',
+               'password': 'domegis'
+            })
+            .end(function (err, res) {
+              if ((!err) && (res.body.token)) {
+                doneBefore();
+              }
+
+              remainingAttempts--;
+
+              if (remainingAttempts > 0) {
+                setTimeout(login, 5000);
+              } else {
+                doneBefore(new Error('Unable to authenticate.'));
+              }
+            });
+      }
+
+      login();
+
     });
   }
 });
