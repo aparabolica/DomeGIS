cb9bc1af60711a46693dadb1b6e09d2ee2703fc7
Miguel Peixe <miguel.peixe@aparabolica.com.br>
2017-07-01 01:49:17 -0300

    avoid updating apt, shrink env into 1 layer and dont compile mapnik (use v3.0.9 from ubuntu rep)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
+echo "Updating permissions..."
+chown -R $APP_USER:$APP_USER /src
+
 # wait for postgres to accept connections
-sleep 30
+sleep 10
 until sequelize db:migrate; do
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 30
+  sleep 5
 done
 
 # allow the container to be started with `--user`
