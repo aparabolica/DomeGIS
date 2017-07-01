c9346abe6badb5284fdfa24446edf33a932f1680
Vitor George <vitor.george@aparabolica.com.br>
2017-07-01 15:13:07 -0300

    Return error if 'style' property is not provided when creating preview (affects #81)

--- src/services/preview/hooks/index.js
+++ src/services/preview/hooks/index.js
@@ -1,23 +1,26 @@
-'use strict';
+"use strict";
 
-var _ = require('underscore');
-var hooks = require('feathers-hooks-common');
-var auth = require('feathers-authentication').hooks;
+var _ = require("underscore");
+var errors = require("feathers-errors");
+var hooks = require("feathers-hooks-common");
+var auth = require("feathers-authentication").hooks;
 
 var setLayergroup = function(hook) {
-  return new Promise(function(resolve, reject){
-    var MapController = hook.app.get('mapController');
-    MapController.getLayerGroupId(hook.data, function(err, layergroupId){
-      if (err) {
-        console.log('error getLayerGroupId');
-        console.log(err);
-        return reject(err);
-      }
+  return new Promise(function(resolve, reject) {
+    var MapController = hook.app.get("mapController");
+
+    if (!hook.data.style)
+      return reject(
+        new errors.BadRequest("Missing 'style' property for preview.")
+      );
+
+    MapController.getLayerGroupId(hook.data, function(err, layergroupId) {
+      if (err) return reject(err);
       hook.data.layergroupId = layergroupId;
       resolve();
     });
   });
-}
+};
 
 exports.before = {
   all: [],
@@ -27,17 +30,19 @@ exports.before = {
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToRoles({ roles: ['editor', 'author'] }),
+    auth.restrictToRoles({ roles: ["editor", "author"] }),
     function(hook) {
-      return new Promise(function(resolve, reject){
-        var Layers = hook.app.service('layers');
+      return new Promise(function(resolve, reject) {
+        var Layers = hook.app.service("layers");
 
         hook.data.creatorId = hook.params.user.id;
 
-        Layers.get(hook.data.layerId).then(function(layer){
-          hook.data.type = layer.type;
-          resolve();
-        }).catch(reject);
+        Layers.get(hook.data.layerId)
+          .then(function(layer) {
+            hook.data.type = layer.type;
+            resolve();
+          })
+          .catch(reject);
       });
     },
     setLayergroup
@@ -46,21 +51,21 @@ exports.before = {
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToRoles({ roles: ['editor', 'author'] }),
+    auth.restrictToRoles({ roles: ["editor", "author"] }),
     setLayergroup
   ],
   patch: [
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToRoles({ roles: ['editor', 'author'] }),
+    auth.restrictToRoles({ roles: ["editor", "author"] }),
     setLayergroup
   ],
   remove: [
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToRoles({ roles: ['editor', 'author'] })
+    auth.restrictToRoles({ roles: ["editor", "author"] })
   ]
 };
 
